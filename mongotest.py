9bee1a0af22f4b511b50f131269148b5fc364bdd
girish1015 <106231713+girish1015@users.noreply.github.com>
2022-07-17 23:54:00 +0530

    changed cluster

--- mongotest.py
+++ mongotest.py
@@ -1,13 +1,11 @@
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://girish1015:Ka023915@girish1015.yhsgo.mongodb.net/?retryWrites=true&w=majority")
+
+client = pymongo.MongoClient("mongodb+srv://home:ka023915@cluster0.yhsgo.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
+
 print(db)
 
-d = {
-    "name": "Girish",
-    "EmailID" : "girish1015@gmail.com",
-    "surname" : "kumar"
-}
+data = {"name": 'girish', "EmailID":"girish1015@gmail.com",  "surname":"kumar"}
 db1 = client['mongotest']
 coll = db1['test']
-coll.insert_one(d)
+coll.insert_one(data)
